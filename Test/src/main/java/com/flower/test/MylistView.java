41b36b226057361f239573c0c12cc51d85a79e2d
flower <scuthua21@gmail.com>
2016-06-11 21:48:54 +0800

    动态生成btn的版本，还可以左滑或者右滑
    动态生成btn的版本，还可以左滑或者右滑

--- Test/src/main/java/com/flower/test/MylistView.java
+++ Test/src/main/java/com/flower/test/MylistView.java
@@ -2,13 +2,17 @@ package com.flower.test;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
@@ -16,12 +20,13 @@ import android.widget.RelativeLayout;
  * Created by flower on 2016/3/26.
  */
 public class MylistView extends ListView {
-    private GestureDetector gestureDetector;
     private Context context;
     private int selectedItem;
     private int previousPositionX;
     private boolean isDeleteShow = false;
     private boolean selcetedItemChange;
+    private int mScreenWidth;
+    private RelativeLayout.LayoutParams params;
 
     private View btn;
     private ViewGroup viewGroup;
@@ -30,23 +35,27 @@ public class MylistView extends ListView {
 
     public MylistView(Context context) {
         super(context);
-        init();
+        init(context);
     }
 
     public MylistView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context);
     }
 
     public MylistView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context);
     }
 
 
-    private void init() {
-//        gestureDetector = new GestureDetector(context, this);
-//        setOnTouchListener(this);
+    private void init(Context context) {
+        this.context = context;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context
+                .WINDOW_SERVICE);
+        DisplayMetrics outMetris = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(outMetris);
+        mScreenWidth = outMetris.widthPixels;
     }
 
     @Override
@@ -54,17 +63,15 @@ public class MylistView extends ListView {
         int presentPositionX;
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                Log.e("tag", "ACTION_DOWN");
                 /*
                 判断当前按下的item和之前的是不是一样
                  */
                 int temp = selectedItem;
                 selectedItem = pointToPosition((int) ev.getX(), (int) ev.getY());
                 if (temp != selectedItem) {
-                    Log.e("tag", "onTouch: caonima");
                     selcetedItemChange = true;
                 } else {
-                    selcetedItemChange=false;
+                    selcetedItemChange = false;
                 }
                 /*
                 当选择了不同的item要将已经显示的btn隐藏删除
@@ -76,47 +83,68 @@ public class MylistView extends ListView {
                 super.onTouchEvent(ev);
                 break;
             case MotionEvent.ACTION_MOVE:
-                Log.e("tag", "ACTION_MOVE");
+                SlideAdd(ev);
                 super.onTouchEvent(ev);
                 break;
             case MotionEvent.ACTION_UP:
-                Log.e("tag", "ACTION_UP");
                 presentPositionX = (int) ev.getX();
-                Log.e("tag", "previousPositionX:" + previousPositionX + "   presentPositionX:" +
-                        presentPositionX);
                 /*
-                当界面上没有button，并且滑动距离向左大于200，显示button
+                当我们点击的还是原来的item但不是在btn上，我们应该让btn消失。当滑动太小的时候也让button消失
                  */
-                if (!isDeleteShow && (presentPositionX - previousPositionX < -200)) {
-                    btn = LayoutInflater.from(getContext()).inflate(R.layout.btn, null);
-                    btn.setOnClickListener(new OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            viewGroup.removeView(btn);
-                            isDeleteShow = false;
-                            btn = null;
-                            onItemDeleteListener.onItemDelete(selectedItem);
-                        }
-                    });
-                    viewGroup = (ViewGroup) getChildAt(selectedItem - getFirstVisiblePosition());
-                    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup
-                            .LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                    params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-                    params.addRule(RelativeLayout.CENTER_VERTICAL);
-                    btn.setLayoutParams(params);
-                    viewGroup.addView(btn);
-                    btnShow(btn);
-                    isDeleteShow = true;
-                    /*
-                    当我们点击的还是原来的item但不是在btn上，我们应该让btn消失。
-                     */
-                } else if (isDeleteShow&&(Math.abs(presentPositionX-previousPositionX)<100)) {
+                if (isDeleteShow && (Math.abs(presentPositionX - previousPositionX) < 100)) {
                     btnHide(btn);
                 } else {
                     super.onTouchEvent(ev);
                 }
         }
         return true;
+    }
+    private void SlideAdd(MotionEvent ev) {
+    /*
+    当界面上没有button并且滑动大于30的时候才开始处理button的显示
+     */
+        if (!isDeleteShow && Math.abs(ev.getX() - previousPositionX) > 30) {
+            btn = LayoutInflater.from(getContext()).inflate(R.layout.btn, null);
+            btn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    viewGroup.removeView(btn);
+                    isDeleteShow = false;
+                    btn = null;
+                    onItemDeleteListener.onItemDelete(selectedItem);
+                }
+            });
+            viewGroup = (ViewGroup) getChildAt(selectedItem - getFirstVisiblePosition());
+            params = new RelativeLayout.LayoutParams(ViewGroup
+                    .LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.addRule(RelativeLayout.CENTER_VERTICAL);
+            btn.setLayoutParams(params);
+            viewGroup.addView(btn);
+            isDeleteShow = true;
+        }
+        /*
+        通过mScreenWidth 和滑动的距离设置btn的margin。动态设置margin是通过params设置的，这点和padding不一样
+        设置滑动距离小于300是为了不让button离开有边界太远，这是是我自己的情况，不一定适用于所有设备。
+         */
+        if (Math.abs(ev.getX() - previousPositionX) > 30 && Math.abs(ev.getX() -
+                previousPositionX) < 300) {
+            if (ev.getX() - previousPositionX < 0) {
+                int left = (int) (mScreenWidth + ev.getX() - previousPositionX);
+                params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                params.setMarginStart(left);
+                params.setMarginEnd(0);
+                btn.setLayoutParams(params);
+            } else {
+                /*
+                set的方法在左右滑动的时候会被覆盖，但是add的方法不会，所以这里需要removeRule
+                 */
+                int right=(int) (mScreenWidth - (ev.getX() - previousPositionX));
+                params.removeRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                params.setMarginEnd(right);
+                params.setMarginStart(0);
+                btn.setLayoutParams(params);
+            }
+        }
     }
 
     /**
@@ -130,132 +158,6 @@ public class MylistView extends ListView {
         this.onItemDeleteListener = onItemDeleteListener;
     }
 
-
-//    @Override
-//    public boolean onTouch(View v, MotionEvent event) {
-//        Log.e("tag", "onTouch: ");
-//        int temp = selectedItem;
-//        selectedItem = pointToPosition((int) event.getX(), (int) event.getY());
-//        if (temp != selectedItem) {
-//            Log.e("tag", "onTouch: caonima");
-//            selcetedItemChange = true;
-//        }
-////        if (isDeleteShow) {
-////            btnHide(btn);
-////            btn.setVisibility(GONE);
-////            isDeleteShow = false;
-////            btn = null;
-////            return true;
-////        } else {
-//        return gestureDetector.onTouchEvent(event);
-//
-////        }
-//
-//    }
-//
-//
-//    @Override
-//    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-//        Log.e("tag ","pmFlomg");
-//        Log.e("tag", "onFling: e1.x= " + e1.getX() + "e2.x= " + e2.getX());
-//        if (!isDeleteShow && (e1.getX() - e2.getX() > 300)) {
-//            btn = LayoutInflater.from(getContext()).inflate(R.layout.btn, null);
-//            btn.setOnClickListener(new OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    viewGroup.removeView(btn);
-//                    isDeleteShow = false;
-//                    btn = null;
-//                    onItemDeleteListener.onItemDelete(selectedItem);
-//                }
-//            });
-//            viewGroup = (ViewGroup) getChildAt(selectedItem - getFirstVisiblePosition());
-//            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup
-//                    .LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-//            params.addRule(RelativeLayout.CENTER_VERTICAL);
-//            btn.setLayoutParams(params);
-//            viewGroup.addView(btn);
-//            btnShow(btn);
-//            isDeleteShow = true;
-//        } else {
-//            setOnTouchListener(this);
-//        }
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean onDown(MotionEvent e) {
-//        Log.e("tag", "onDown: ");
-//        if (!isDeleteShow) {
-//            selectedItem = pointToPosition((int) e.getX(), (int) e.getY());
-//
-//        } else {
-//            viewGroup.removeView(btn);
-//            isDeleteShow = false;
-//        }
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-//        Log.e("tag ","onScroll");
-//        Log.e("tag", "onScroll: ");
-//        int offset = ((int) e2.getX() - (int) e1.getX());
-//        if (offset < -50) {
-//            if (btn == null || (btn!=null&&selcetedItemChange)) {
-//                Log.e("tag", "onScroll: ????");
-//                btn = null;
-//                btn = LayoutInflater.from(getContext()).inflate(R.layout.btn, null);
-//                btn.setOnClickListener(new OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        viewGroup.removeView(btn);
-//                        isDeleteShow = false;
-//                        btn = null;
-//                        onItemDeleteListener.onItemDelete(selectedItem);
-//                    }
-//                });
-//                viewGroup = (ViewGroup) getChildAt(selectedItem - getFirstVisiblePosition());
-//                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup
-//                        .LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//                params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-//                params.addRule(RelativeLayout.CENTER_VERTICAL);
-//                btn.setLayoutParams(params);
-//                btn.setPadding(btn.getPaddingLeft(), btn.getPaddingTop(), btn.getPaddingRight
-//                        (), btn
-//                        .getPaddingBottom());
-//                viewGroup.addView(btn);
-//                isDeleteShow = true;
-//                selcetedItemChange = false;
-//            }
-//            if (offset > -200) {//大于-200是用于限制button增加的太大，小于-50是保证向左滑动才会出现这个button
-//                Log.e("tag", "onScroll: offset= " + offset);
-//                Log.e("tag", "onScroll: btnmeasurewidth= " + btn.getMeasuredWidth());
-//                btn.setPadding(btn.getPaddingLeft(), btn.getPaddingTop(), -btn.getMeasuredWidth
-// () -
-//                        offset, btn
-//                        .getPaddingBottom());
-//
-//            }
-//        }
-//
-//        return false;
-//    }
-//
-//    @Override
-//    public void onShowPress(MotionEvent e) {
-//    }
-//
-//    @Override
-//    public boolean onSingleTapUp(MotionEvent e) {
-//        return false;
-//    }
-//
-//    @Override
-//    public void onLongPress(MotionEvent e) {
-//
-//    }
 
     private void btnShow(View v) {
         v.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.btn_show));
